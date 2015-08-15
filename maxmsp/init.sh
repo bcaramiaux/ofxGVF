aa202fcb3b48773eff4b2e606f791355ca0f6c9b
bcaramiaux <baptiste.caramiaux@gmail.com>
2015-08-15 13:01:55 +0100

    delete derived data

--- maxmsp/init.sh
+++ maxmsp/init.sh
@@ -3,12 +3,10 @@
 
 MaxSDK="max6-sdk"
 
-echo "Cloning max sdk"
-
 if [ -d "$MaxSDK" ]; then
 	rm -rf $MaxSDK
 fi
-
+echo "Cloning max sdk"
 git clone --depth 1 https://github.com/Cycling74/max6-sdk.git
 
 
